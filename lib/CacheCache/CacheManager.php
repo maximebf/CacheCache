02e05ea455f293179a4095895dbf00956cc34aa0
maximebf <maxime.bouroumeau@gmail.com>
2012-08-18 10:14:51 +0200

    updated copyright notice

--- lib/CacheCache/CacheManager.php
+++ lib/CacheCache/CacheManager.php
@@ -1,19 +1,11 @@
 <?php
-/**
- * CacheCache
- * Copyright (c) Maxime Bouroumeau-Fuseau
+/*
+ * This file is part of the MetaP package.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) 2012 Maxime Bouroumeau-Fuseau
  *
- * @author Maxime Bouroumeau-Fuseau
- * @copyright (c) Maxime Bouroumeau-Fuseau
- * @license http://www.opensource.org/licenses/mit-license.php
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace CacheCache;
